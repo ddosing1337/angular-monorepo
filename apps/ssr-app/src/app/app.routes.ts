42a31a23b320a2a06c48b5e35cef3a89d42fffe6
Ivan Parfinenka <ivan.parfinenka@innowise.com>
2025-05-06 16:16:09 +0300

    init: ssr-app initial commit

---
+++ apps/ssr-app/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const appRoutes: Route[] = [{ path: 'home', component: HomeComponent }];
